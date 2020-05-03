5bfbb93aa83b5efccd9de564b766b8f5e1b54462
Rayjay-8 <rayarruda9876@gmail.com>
2020-05-03 13:13:11 -0300

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 import os
 #informe na lista os modulos para instalar
-list_of_updates = ["asdfcxwer","github","desktopmagic","keyboard","pyautogui","cv2"]
+list_of_updates = ["opencv-python","PyGithub","desktopmagic","keyboard","pyautogui","cv2"]
 def updates_important():
   global list_of_updates
   for i in list_of_updates:
