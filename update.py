d478b02bdfe3c7335537ec4d9bdfe9eff2d8dc02
Rayjay-8 <rayarruda9876@gmail.com>
2020-05-03 13:01:00 -0300

    Update update.py

--- update.py
+++ update.py
@@ -1,11 +1,13 @@
 import os
-
+#informe na lista os modulos para instalar
 list_of_updates = ["lxml"]
-
 def updates_important():
+  global list_of_updates
   for i in list_of_updates:
-    ret = os.system(i)
+    ret = os.system("pip install "+i)
+    #ret = os.system("pip uninstall "+i)
     if ret != 0:
       print("updates is ok!!!")
-     else:
+    else:
       print("erro ao realizar a instalacao")
+updates_important()
