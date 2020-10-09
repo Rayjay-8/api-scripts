4460c2ed89c8f3295f82cbe1323309c51c049f78
Rayjay-8 <rayarruda9876@gmail.com>
2020-10-09 16:24:46 -0300

    Add files via upload

---
+++ garante_inicio.py
@@ -0,0 +1,24 @@
+
+import time
+import pyautogui
+time.sleep(3)
+
+
+pyautogui.click(10,10)
+
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+pyautogui.press("esc")
+
+pyautogui.press("enter")
+pyautogui.hotkey('shift','tab')
+pyautogui.press("enter")
